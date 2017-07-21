c1d70f1171946736a1f874eaa0e822c3a4099f30
sandippatidar89 <sandip8softworld@gmail.com>
2017-07-21 20:53:37 +0530

    Create PalindromeTest.cpp
    Palindrome number test code for the interview.

---
+++ PalindromeTest.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+
+
+using namespace std;
+
+void isPalindrome(int num)
+{
+ 	int tmp, final = 0;
+ 	int test =  num;
+ 	while(num > 0)
+ 	{
+ 		tmp =  num % 10;
+ 		final =  final*10 + tmp;
+ 		num =  num/10;
+ 	}
+ 	
+ 	if(final ==  test)
+ 	{
+ 		cout<< "Nuber" << test << " is Palindrom Number" << endl;
+	}
+	else
+	{
+		cout<< "Nuber " << test << " is NOT Palindrom Number" << endl;
+	}
+}
+int main()
+{
+	isPalindrome(123321);
+	isPalindrome(125);
+	isPalindrome(1212);
+	isPalindrome(121);		
+	return 0;
+}
